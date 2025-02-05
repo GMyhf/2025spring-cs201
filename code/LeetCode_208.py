0af6fac63a1bba704eba38ee622ebed703de10eb
Hongfei Yan <yanhf@pku.edu.cn>
2025-02-05 17:08:39 +0800

    initial

---
+++ code/LeetCode_208.py
@@ -0,0 +1,51 @@
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.end_of_word = "#"
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})    #returns the value of the item with the specified key.
+                                                # If the key does not exist, insert the key, with the specified value
+        node[self.end_of_word] = self.end_of_word
+
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return self.end_of_word in node
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+
+if __name__ == "__main__":
+    trie = Trie()
+    trie.insert("apple")
+    print(trie.search("apple"))   # return True
+    print(trie.search("app"))     # return False
+    print(trie.startsWith("app")) # return True
+    trie.insert("app")
+    print(trie.search("app"))     # return True
+
